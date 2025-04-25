1ec68e5c69afc26199d13e7fb592fd946097e969
alhadh7 <alhadh707@gmail.com>
2025-04-25 17:10:17 +0530

    25-04-20 booking glitch fix again - 4

--- Next_App/authentication/serializers.py
+++ Next_App/authentication/serializers.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from rest_framework import serializers
 from authentication.models import CustomUser, Partner, ServiceType, Booking, BookingRequest, BookingExtension, Review
 
